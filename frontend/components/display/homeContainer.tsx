88cf4e9a2567cc2d3cc4da41a5ef7e647fe8fc4b
shunsuke <kimushun1226.634@gmail.com>
2025-02-15 13:38:01 +0900

    ブランチ作成

--- frontend/components/display/homeContainer.tsx
+++ frontend/components/display/homeContainer.tsx
@@ -1,14 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
 const HomeContainer: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
   return <div>HomeContainer</div>;
 };
 
