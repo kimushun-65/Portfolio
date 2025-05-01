2eb0dfc4ae6d4fd8a02f251d2cdb30a51eb3fa1f
shunsuke <kimushun1226.634@gmail.com>
2025-05-01 10:50:14 +0900

    summary

--- frontend/components/display/summarySection.tsx
+++ frontend/components/display/summarySection.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 const SummarySection: React.FC = () => {
   return (
     <div className='section summary'>
-      <h2>Summary</h2>
-      <p>
-        新卒以来2年以上フロントエンドエンジニアとして活動し、実務経験を積んできました。
+      <h2 className='text-3xl font-bold'>Summary</h2>
+      <p className='font-semibold'>
+        普段は大学院で機械学習を用いた金融市場の暴落検知の研究をしています。
       </p>
-      <p>
-        主に学術系のWebアプリケーションの開発に携わり、ユーザーの研究活動をより効率的にするためのUI/UX設計・実装を行ってきました。
+      <p className='font-semibold'>
+        学業以外では1年ほどエンジニアをしており、主にフロントエンドの開発を行っています。
+        設計から作成テストまで一通りの開発を経験しています。
+      </p>
+      <p className='font-semibold'>
+        今後はバックエンドやインフラの知識を深め、フルスタックエンジニアとしてのスキルを高めていきたいと考えています。将来的には学んだ知識を活かして革新的なサービスを作りたいと思ってます。
       </p>
     </div>
   );
--- frontend/components/display/workExperienceSection.tsx
+++ frontend/components/display/workExperienceSection.tsx
@@ -1,9 +1,11 @@
 export default function WorkExperienceSection() {
   return (
     <div className='section work'>
-      <h2>Work Experience</h2>
+      <h2 className='text-3xl font-bold'>Work Experience</h2>
       <div className='timeline-item'>
-        <div className='mb-2 text-sm text-blue-200'>2024/10〜現在</div>
+        <div className='mb-2 text-lg font-semibold text-blue-200'>
+          2024/10〜現在
+        </div>
         <div className='company'>株式会社 STAR UP</div>
         <p>
           研究者向けWebアプリ開発に従事。主にフロントエンドを担当している。
