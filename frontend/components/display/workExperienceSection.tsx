c474acf865ab065cb752c5386f6dbf7d0e3612bc
shunsuke <kimushun1226.634@gmail.com>
2025-05-01 11:05:29 +0900

    workexperience

--- frontend/components/display/workExperienceSection.tsx
+++ frontend/components/display/workExperienceSection.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function WorkExperienceSection() {
   return (
     <div className='section work'>
@@ -6,10 +8,21 @@ export default function WorkExperienceSection() {
         <div className='mb-2 text-lg font-semibold text-blue-200'>
           2024/10〜現在
         </div>
-        <div className='company'>株式会社 STAR UP</div>
-        <p>
-          研究者向けWebアプリ開発に従事。主にフロントエンドを担当している。
-          ユーザーの研究活動をサポートし、データの可視化や分析機能の実装を行っている。
+        <div className='company flex items-center font-semibold'>
+        <Image
+            src='/images/starup.png'
+            alt='Star Up Logo'
+            width={32}
+            height={32}
+            className='ml-2 rounded-md mr-3'
+          />
+          株式会社 STAR UP
+        </div>
+        <p className='font-semibold mt-2'>
+          受託開発事業部に在籍し、主にフロントエンドの開発を行っている。
+        </p>
+        <p className='font-semibold'>
+          需要予測を組み込んだ発注ツールの開発を行っており、フロントエンドチームのリーダー兼PM補佐を務めている。またバックエンド開発にも一部携わっており幅広く活躍している。
         </p>
       </div>
     </div>
